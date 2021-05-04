1572587cac6257163162432182c928781acd2a37
Shorouk Roshdy <shoroukroshdy10@gmail.com>
2021-05-04 07:31:02 +0200

    Static Search Page & some login modification

--- lib/Models/user_model.dart
+++ lib/Models/user_model.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-
 class User {
   String username;
   String followers;
--- lib/Views/login.dart
+++ lib/Views/login.dart
@@ -2,10 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
 import 'sign_up.dart';
- const style =TextStyle(
-    fontFamily: 'ProximaNova', fontWeight: FontWeight.bold,
- );
 
+const style =TextStyle(
+  fontFamily: 'ProximaNova', fontWeight: FontWeight.bold,
+);
 void main() {
   return runApp(
     MaterialApp(
@@ -14,25 +14,25 @@ void main() {
         appBar: AppBar(
           backgroundColor:Color(0xFF212124) ,
           title: Row (
-            mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Image(
-              image: AssetImage("assets/flickr-logo.png"),
-              height: 20,
-              width: 60,
-            ),
-          Text(
-              'flickr',
-            style: TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 30.0,
-              fontFamily: 'Frutiger',
-            )
-          ),
-        ]
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Image(
+                  image: AssetImage("assets/flickr-logo.png"),
+                  height: 20,
+                  width: 60,
+                ),
+                Text(
+                    'flickr',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 30.0,
+                      fontFamily: 'Frutiger',
+                    )
+                ),
+              ]
           ),
         ),
-         body: Login(),
+        body: Login(),
       ),
     ),
   );
@@ -44,27 +44,27 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-    bool _showWidgets=false;
-    String _submitTitle='Next';
-    String _email;
-    bool _hidePassword=true;
-    bool valueFirst = false;
-    // bool valueSecond = false;
-    wrongEmailAlert(BuildContext context)
+  bool _showWidgets=false;
+  String _submitTitle='Next';
+  String _email;
+  bool _hidePassword=true;
+  bool valueFirst = false;
+  // bool valueSecond = false;
+  wrongEmailAlert(BuildContext context)
   {
-      return showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('Hmm... that\'s not an email address'),
-              content: Text('Please enter your complete Flickr account email address (e.g name@domain.com). If you still log in through yahoo, click below to continue.'),
-              actions: <Widget>[
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Hmm... that\'s not an email address'),
+            content: Text('Please enter your complete Flickr account email address (e.g name@domain.com). If you still log in through yahoo, click below to continue.'),
+            actions: <Widget>[
               TextButton(
                 child: Text('Continue to Yahoo'),
               ),
               TextButton(
                 onPressed: (){
-                Navigator.of(context).pop();
+                  Navigator.of(context).pop();
                 },
                 child: Text('Try Again'),
                 style: TextButton.styleFrom(
@@ -73,22 +73,24 @@ class _LoginState extends State<Login> {
                 ),
               ),
             ],
-            );
-          }
-      );
+          );
+        }
+    );
   }
-   checkEmail(_email)
+  checkEmail()
   {
+    _email=_email.trim();
+    print(_email);
     bool check=EmailValidator.validate(_email);
     if (check==true)
-      {
-        _showWidgets=true;
-        _submitTitle='Sign in';
-      }
+    {
+      _showWidgets=true;
+      _submitTitle='Sign in';
+    }
     else
-      {
-        wrongEmailAlert(context);
-      }
+    {
+      wrongEmailAlert(context);
+    }
   }
   @override
   Widget build(BuildContext context) {
@@ -97,25 +99,25 @@ class _LoginState extends State<Login> {
       child: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-              Image(
-                image: AssetImage("assets/flickr-logo.png"),
-                height: 17,
-                width: 55,
-              ),
+            Image(
+              image: AssetImage("assets/flickr-logo.png"),
+              height: 17,
+              width: 55,
+            ),
             SizedBox(
               height: 10.0,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-            Text(
-            'Log in to Flickr',
-              style:TextStyle(
-                fontFamily: 'ProximaNova',
-                fontSize: 22.0,
-                fontWeight: FontWeight.bold,
-              )
-            ),
+                Text(
+                    'Log in to Flickr',
+                    style:TextStyle(
+                      fontFamily: 'ProximaNova',
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                    )
+                ),
               ],
             ),
             SizedBox(
@@ -125,21 +127,24 @@ class _LoginState extends State<Login> {
               padding: EdgeInsets.only(left:20.0,right:20.0 ),
               child: TextField(
                 onChanged: (email){
-                  print('ana hena');
                   _email=email;
-                   },
-                 onSubmitted: (email){
+                },
+                onSubmitted: (email){
                   setState(() {
-                    checkEmail(_email);
+                    if (_email!="" && _email!=null) {
+                      checkEmail();
+                      FocusScope.of(context).requestFocus(FocusNode());
+                    }
+
                   });
                 },
                 decoration: InputDecoration(
-                  labelText:'Email address',
-                  labelStyle: style,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(3.0)),
-                    borderSide: BorderSide(color:Colors.pink),
-                  )
+                    labelText:'Email address',
+                    labelStyle: style,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                      borderSide: BorderSide(color:Colors.pink),
+                    )
                 ),
               ),
             ),
@@ -155,40 +160,40 @@ class _LoginState extends State<Login> {
                     autocorrect: false,
                     obscureText: _hidePassword,
                     decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          Icons.remove_red_eye,
-                          color: this._hidePassword ? Colors.grey : Colors.blue,
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            Icons.remove_red_eye,
+                            color: this._hidePassword ? Colors.grey : Colors.blue,
+                          ),
+                          onPressed: () {
+                            setState(() => this._hidePassword = !this._hidePassword);
+                          },
                         ),
-                        onPressed: () {
-                          setState(() => this._hidePassword = !this._hidePassword);
-                        },
-                      ),
-                      labelText:'Password',
-                      labelStyle: style,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(3.0)),)
-                      )
-                  ),
+                        labelText:'Password',
+                        labelStyle: style,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(3.0)),)
+                    )
                 ),
               ),
+            ),
             Visibility(
               visible: _showWidgets,
               child: Row(
-                children:<Widget> [
-                  Container(
-                    padding: EdgeInsets.only(left:5.0),
-                    child: Checkbox(
-                    value: this.valueFirst,
-                    onChanged: (bool value) {
-                      setState(() {
-                        this.valueFirst = value;
-                      });
-                    }
-                ),
-                  ),
-                  Text('Remember email address', style:style),
-                ]
+                  children:<Widget> [
+                    Container(
+                      padding: EdgeInsets.only(left:5.0),
+                      child: Checkbox(
+                          value: this.valueFirst,
+                          onChanged: (bool value) {
+                            setState(() {
+                              this.valueFirst = value;
+                            });
+                          }
+                      ),
+                    ),
+                    Text('Remember email address', style:style),
+                  ]
               ),
             ),
             SizedBox(
@@ -198,12 +203,13 @@ class _LoginState extends State<Login> {
               padding: EdgeInsets.only(left:20.0,right:20.0),
               width: double.infinity ,
               height: 40.0,
-             child: TextButton(
-              onPressed: (){
-                setState(() {
-                    checkEmail(_email);
-                });
-              },
+              child: TextButton(
+                onPressed: (){
+                  setState(() {
+                    checkEmail();
+                    FocusScope.of(context).requestFocus(FocusNode());
+                  });
+                },
                 child: Text(
                   _submitTitle,
                   style: TextStyle(
@@ -212,32 +218,31 @@ class _LoginState extends State<Login> {
                     fontSize: 17.0,
                   ),
                 ),
-               style: TextButton.styleFrom(
-                 primary: Colors.white,
-                 backgroundColor: Colors.blue,
-               ),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.blue,
+                ),
               ),
             ),
             Visibility(
                 visible: _showWidgets,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget> [
-                  TextButton(
-                    onPressed:(){
-                      //TODO: Navigate to forgot password page, & pass the email to it.
-                    },
-                    child: Text('Forgot passowrd?',style:style),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(left:20.0,right:20.0),
-                    child: Divider(
-                        color: Colors.black,
-
-                    ),
-                  )
-                ]
-              )
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget> [
+                      TextButton(
+                        onPressed:(){
+                          //TODO: Navigate to forgot password page, & pass the email to it.
+                        },
+                        child: Text('Forgot passowrd?',style:style),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(left:20.0,right:20.0),
+                        child: Divider(
+                          color: Colors.black,
+                        ),
+                      )
+                    ]
+                )
             ),
             Visibility(
               visible: !_showWidgets,
@@ -246,27 +251,27 @@ class _LoginState extends State<Login> {
               ),
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget> [
-                Text('Not a Flickr member?', style: style),
-                TextButton(
-                    onPressed: (){
-                      //TODO: Navigate to sign up page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context)  {
-                        return SignUp();
-                        },
-                      ),
-                      );
-                    },
-                    child: Text(' Sign up here.', style:style)
-                  // style :
-                )
-              ]
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget> [
+                  Text('Not a Flickr member?', style: style),
+                  TextButton(
+                      onPressed: (){
+                        //TODO: Navigate to sign up page
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context)  {
+                              return SignUp();
+                            },
+                          ),
+                        );
+                      },
+                      child: Text(' Sign up here.', style:style)
+                    // style :
+                  )
+                ]
             )
-        ],
+          ],
         ),
       ),
     );
