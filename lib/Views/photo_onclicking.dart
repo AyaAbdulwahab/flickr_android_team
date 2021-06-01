ae0037d7835c4c7dc48befa8b557a009cf613f8b
AalaaMohamed <aalaasalaheldin.99@gmail.com>
2021-06-02 01:23:46 +0200

    Photo On Clicking

--- lib/Views/photo_onclicking.dart
+++ lib/Views/photo_onclicking.dart
@@ -1,15 +1,31 @@
 import 'package:flickr/Widgets/post_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flickr/Widgets/faves_and_comments.dart';
+import 'package:flickr/Views/photo_details.dart';
 
+/////HOW TO USE THE FUNCTIONS WITHIN A DIFFERENT CLASS (ADD AND REMOVES FAVES)
+
+///The [PhotoOnClicking] takes the required data about the photo and views the photo
 class PhotoOnClicking extends StatefulWidget {
-  String image, userImage, username, caption, faves, comments;
+  String image, userImage, username, userRealName, userID, description, dateTaken, caption, views, faves, comments;
+  int safety;
+  bool privacy;
+  List<dynamic> tags;
   PhotoOnClicking(
       {@required this.image,
-      @required this.userImage,
-      @required this.username,
-      @required this.caption,
-      @required this.faves,
-      @required this.comments});
+        @required this.userImage,
+        @required this.username,
+        @required this.userID,
+        @required this.userRealName,
+        @required this.privacy,
+        @required this.safety,
+        @required this.views,
+        @required this.dateTaken,
+        @required this.caption,
+        @required this.description,
+        @required this.tags,
+        @required this.faves,
+        @required this.comments});
   Icon s=Icon(Icons.star_border_outlined, color: Colors.white,);
   @override
   _PhotoOnClickingState createState() => _PhotoOnClickingState();
@@ -50,7 +66,7 @@ class _PhotoOnClickingState extends State<PhotoOnClicking> {
               ),
               ListTile(
                 title:
-                    Text(widget.caption, style: TextStyle(color: Colors.white)),
+                Text(widget.caption, style: TextStyle(color: Colors.white)),
               ),
               Divider(height: 1.0, color: Colors.grey[200]),
               Row(
@@ -62,15 +78,17 @@ class _PhotoOnClickingState extends State<PhotoOnClicking> {
                       children: [
                         IconButton(
                             icon:
-                              widget.s,
+                            widget.s,
                             onPressed: () {
                               setState(() {
                                 if (widget.s.icon==Icons.star_border_outlined)
                                 {
                                   widget.s=Icon(Icons.star, color: Colors.white);
+                                  // addFave();
                                 }
                                 else if (widget.s.icon==Icons.star){
                                   widget.s=Icon(Icons.star_border_outlined, color: Colors.white);
+                                  // removeFave();
                                 }
                               });
                             }),
@@ -82,7 +100,21 @@ class _PhotoOnClickingState extends State<PhotoOnClicking> {
                             onPressed: () {}),
                         IconButton(
                             icon: Icon(Icons.info_outline, color: Colors.white),
-                            onPressed: () {}),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => PhotoDetails(title: widget.caption,
+                                      description: widget.description,
+                                      userRealName:widget.userRealName,
+                                      userID:widget.userID,
+                                      dateTaken: widget.dateTaken,
+                                      tags:widget.tags,
+                                      privacy:widget.privacy,
+                                      views:widget.views,
+                                      safety:widget.safety,)),
+                              );
+                            }),
                       ],
                     ),
                   ),
