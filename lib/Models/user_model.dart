83f699bf587eb67774ae6b7556f59dcceb73f480
Shorouk Roshdy <shoroukroshdy10@gmail.com>
2021-06-01 06:13:48 +0200

    Fully-functional forgot_passowrd page

--- lib/Models/user_model.dart
+++ lib/Models/user_model.dart
@@ -87,3 +87,117 @@ class PrivacyInfo {
     );
   }
 }
+
+
+// To parse this JSON data, do
+//
+//     final welcome = welcomeFromJson(jsonString);
+
+
+
+// To parse this JSON data, do
+//
+//     final welcome = welcomeFromJson(jsonString);
+
+
+
+
+// String welcomeToJson(AllUsers data) => json.encode(data.toJson());
+
+
+
+class SearchedUser {
+  SearchedUser({
+    @required this.userID,
+    @required this.username,
+    @required this.photoCount,
+    @required this.followerCount,
+  });
+
+  String userID;
+  String username;
+  String photoCount;
+  String followerCount;
+
+  factory SearchedUser.fromJson(Map<String, dynamic> json) => SearchedUser(
+    userID: json["_id"],
+    username: json["displayName"],
+    photoCount: json["photoCount"],
+    followerCount: json["followerCount"],
+  );
+
+  static List<SearchedUser> parseList(List<dynamic> list) {
+    return list.map((i) => SearchedUser.fromJson(i)).toList();
+  }
+
+  // Map<String, dynamic> toJson() => {
+  //   "_id": userID,
+  //   "displayName": username,
+  //   "photoCount": photoCount,
+  //   "followerCount": followerCount,
+  // };
+}
+
+
+class SearchedPhoto {
+  SearchedPhoto({
+    this.originalHeight,
+    this.originalWidth,
+    this.originalSource,
+    this.originalURL,
+    this.thumbHeight,
+    this.thumbWidth,
+    this.thumbSource,
+    this.thumbURL,
+    this.favs,
+    this.comments,
+    this.canDownload,
+    this.title,
+    this.descrip,
+    this.username,
+    this.dateTaken,
+    this.pro
+  });
+
+  int originalHeight;
+  int originalWidth;
+  int thumbHeight;
+  int thumbWidth;
+  int favs;
+  int comments;
+  String originalSource;
+  String originalURL;
+  String thumbSource;
+  String thumbURL;
+  String title;
+  String descrip;
+  String dateTaken;
+  String username;
+  bool canDownload;
+  bool pro;
+
+
+  factory SearchedPhoto.fromJson(Map<String, dynamic> json) =>
+      SearchedPhoto(
+        originalHeight: json['sizes']['size']['original']['height'],
+        originalWidth: json['sizes']['size']['original']['width'],
+        thumbHeight: json['sizes']['size']['thumbnail']['height'],
+        thumbWidth: json['sizes']['size']['thumbnail']['width'],
+        favs: json['favourites'],
+        comments: json['comments'],
+        originalSource: json['sizes']['size']['original']['source'],
+        originalURL: json['sizes']['size']['original']['url'],
+        thumbSource: json['sizes']['size']['thumbnail']['source'],
+        thumbURL: json['sizes']['size']['original']['url'],
+        title: json['title'],
+        descrip: json['description'],
+        dateTaken: json['dateTaken'],
+        username: json['userId']['firstName'],
+        canDownload: json['sizes']['size']['canDownload'],
+        pro: json['userId']['pro'],
+      );
+
+  static List<SearchedPhoto> parseList(List<dynamic> list) {
+    return list.map((i) => SearchedPhoto.fromJson(i)).toList();
+  }
+}
---
+++ lib/Views/forgot_password.dart
@@ -0,0 +1,229 @@
+import 'package:flickr/View_Model/user_view_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flickr/Widgets/authentication_app_bar.dart';
+import 'dart:convert';
+
+
+void main() {
+  return runApp(
+      MaterialApp(
+          home: ForgotPassword()
+      )
+  );
+}
+
+const String email="something@gmail.com";
+
+
+//TODO: initialize _email with passed string from login page
+
+/// The [ForgotPassword] page is where user enters their email to send an email for instructions on how to reset their password
+class ForgotPassword extends StatefulWidget {
+  @override
+  _ForgotPasswordState createState() => _ForgotPasswordState();
+}
+
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+
+  String _email="something@gmail.com";
+  bool _invalidAlert=false;
+  final _formKey = GlobalKey<FormState>();
+
+  sendEmail() async {
+    var response = await forgotPassword(_email);
+    // var responseBody = jsonDecode(response.data);
+    if (response.statusCode==200)
+      {
+        print("Navigate");
+        //TODO: Navigate to email_verification page
+      }
+    else
+      {
+        setState(() {
+          _invalidAlert=true;
+        });
+        print("Invalid");
+
+      }
+  }
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: authAppBar(context),
+        body: Form(
+          key: _formKey,
+          child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:<Widget>[
+                  SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        Icon(
+                          Icons.mail,
+                          color: Colors.grey.shade600,
+                          size: 30.0,
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          "Change your Flickr password",
+                          style: TextStyle(
+                              fontFamily: 'ProximaNova',
+                              fontSize:19.0,
+                              fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        SizedBox(
+                          height: 7.0,
+                        ),
+                        Text(
+                          "Please enter your email address below and",
+                          style: TextStyle(
+                              fontFamily: 'ProximaNova',
+                              fontSize:17.0,
+                              fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        Text(
+                          "we'll send you instructions on how to reset your",
+                          style: TextStyle(
+                              fontFamily: 'ProximaNova',
+                              fontSize:17.0,
+                              fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        Text(
+                          "password.",
+                          style: TextStyle(
+                              fontFamily: 'ProximaNova',
+                              fontSize:17.0,
+                              fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        Visibility(
+                          visible: _invalidAlert,
+                          child: Container(
+                            width: 370.0,
+                            height: 40.0,
+                            decoration: BoxDecoration(
+                                color: Colors.red.shade100,
+                                border: Border.all(
+                                  color: Colors.red[100],
+                                ),
+                                borderRadius: BorderRadius.all(Radius.circular(4))),
+                            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                            child: Center(
+                              child: Text(
+                                "Invalid email or password",
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Container(
+                          width: 360,
+                          // height:50,
+                          // padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                          child: TextFormField(
+                            initialValue: _email,
+                            key: Key("forgot-pass-field"),
+                            onChanged: (email) {
+                              setState(() {
+                                _email = email;
+                                _invalidAlert=false;
+                              });
+                            },
+                            onFieldSubmitted: (email) {
+                              if (_formKey.currentState.validate()) {}
+                              setState(() {
+                                if (_email!="") {
+                                  sendEmail();
+                                  FocusScope.of(context).requestFocus(FocusNode());
+                                }
+                              });
+
+                            },
+                            validator: (val) => val.isEmpty
+                                ? "Required"
+                                : null,
+                            decoration: InputDecoration(
+                                contentPadding:EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                                labelText: 'Email address',
+                                // labelStyle: style,
+                                border: OutlineInputBorder(
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(3.0)),
+                                  borderSide: BorderSide(color: Colors.pink),
+                                )),
+                          ),
+                        ),
+
+                        SizedBox(
+                          height: 30.0,
+                        ),
+                        Container(
+                          // padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                          width: 360.0,
+                          height: 40.0,
+                          child: TextButton(
+                            onPressed: (){
+                              if (_formKey.currentState.validate()) {}
+                                //TODO: Request
+                              if (_email!="") {
+                                sendEmail();
+                                FocusScope.of(context).requestFocus(FocusNode());
+                              }
+                            },
+                            child: Text(
+                                "Send email",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15.0,
+                                )
+                            ),
+                            style: TextButton.styleFrom(
+                              primary: Colors.white,
+                              backgroundColor: Colors.blue,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        InkWell(
+                          child: Text(
+                              "Can't access your email?",
+                              style: TextStyle(
+                                  fontFamily: 'ProximaNova',
+                                  fontSize: 15.0,
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold
+                              )
+                          ),
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+          ),
+        )
+    );
+  }
+}
